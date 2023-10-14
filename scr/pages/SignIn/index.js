eeb496ca7f42ab7d2ee22f1eea00451353c40831
Layla Paula <layhpaula@gmail.com>
2023-10-13 22:58:07 -0300

    refactor- Página de Login

--- scr/pages/SignIn/index.js
+++ scr/pages/SignIn/index.js
@@ -1,10 +1,46 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Platform } from 'react-native';
+
+import { Background,
+         Container,
+         AreaInput,
+         Input,
+         SubmitButton,
+         SubmitText,
+         Link,
+         LinkText
+
+     } from './styles';
+
+import { useNavigation } from '@react-navigation/native';
 
 export default function SignIn(){
+  const navigation = useNavigation();
   return(
-    <View>
-      <Text>TELA LOGIN</Text>
-    </View>
+    <Background>
+      <Container
+        behavior = {Platform.OS === 'ios' ? 'padding' : ''}
+        enabled
+      >
+        <AreaInput>
+        <Input 
+          placeholder= "Insira o seu endereço de email"
+        />
+        </AreaInput>
+        <AreaInput>
+        <Input 
+          placeholder= "Coloque a sua senha"
+        />
+        </AreaInput>
+
+        <SubmitButton activeOpacity = {0.8}>
+          <SubmitText>Entrar</SubmitText>
+        </SubmitButton>
+
+        <Link onPress = {() => navigation.navigate('SignUp')}>
+         <LinkText>Cadastre-se</LinkText>
+        </Link>
+      </Container>
+    </Background>
   )
 }
---
+++ scr/pages/SignIn/styles.js
@@ -0,0 +1,49 @@
+import styled from 'styled-components'
+
+export const Background = styled.View`
+   flex:1;
+   background-color: #051037;
+`;
+
+export const Container = styled.KeyboardAvoidingView`
+   flex:1;
+   align-items: center;
+   justify-content: center;
+
+`;
+
+export const AreaInput = styled.View`
+  flex-direction:row;
+`;
+
+export const Input = styled.TextInput`
+ background-color: #F0F0F0;
+ width: 90%;
+ font-size:20px;
+ padding:10px;
+ border-radius: 30px;
+ color: #051037;
+ margin-bottom:15px;
+`;
+
+export const SubmitButton = styled.TouchableOpacity`
+  width: 90%;
+  height:45px;
+  border-radius:8px;
+  background-color:#F9CD2F;
+  margin-top:10px;
+  align-items: center;
+  justify-content:center;
+`;
+export const SubmitText = styled.Text`
+  font-size:20px;
+  color:#FFF;
+`;
+export const Link = styled.TouchableOpacity`
+  margin-top: 10px;
+  margin-bottom:10px;
+
+`;
+export const LinkText = styled.Text`
+ color: #00CF7F
+`;
