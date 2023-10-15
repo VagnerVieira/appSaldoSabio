c4fe9473f0e79a03f0491ded885b3b4d6681c5d5
Layla Paula <layhpaula@gmail.com>
2023-10-14 21:10:50 -0300

    refactor- Tela Home

--- scr/routes/app.routes.js
+++ scr/routes/app.routes.js
@@ -7,7 +7,23 @@ const AppDrawer = createDrawerNavigator();
 
 function AppRoutes(){
   return(
-    <AppDrawer.Navigator>
+    <AppDrawer.Navigator
+      screenOptions={{
+        headerShown:false,
+
+        drawerStyle:{
+          backgroundColor: '#F9CD2F',
+          paddingTop: 20,
+        },
+
+        drawerActiveBackgroundColor:'#0F0F18',
+        drawerActiveTintColor: '#FFF',
+
+        drawerInactiveBackgroundColor: '#F0F2FF',
+        drawerInactiveTintColor: '#121212'
+
+      }}
+    >
       <AppDrawer.Screen
         name="Home"
         component={Home}
