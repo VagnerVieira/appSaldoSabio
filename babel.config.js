73029248964c3d13e0de4836a1a99ccf49d199c7
Layla Paula <layhpaula@gmail.com>
2023-10-18 10:33:17 -0300

    refactor- dependencias

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,8 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-<<<<<<< HEAD
-=======
     plugins: [
       'react-native-reanimated/plugin',
     ],
->>>>>>> 614810b24d01ef2d5b11d78ef4161a4b3bdaf14b
   };
 };
